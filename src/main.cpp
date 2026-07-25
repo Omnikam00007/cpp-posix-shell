b50343031760bebbc24d0685095b2eb6b6d9f61c
Om Nikam <omnikam00007@gmail.com>
2026-07-25 11:03:46 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,69 @@
 #include <iostream>
+#include <algorithm>
+#include <format>
+#include <iterator>
+#include <unistd.h>
 #include <string>
+#include <cstdlib>
+#include <vector>
+#include <sstream>
+#include <filesystem>
+class Shell{
+public:
+  std::string command;
+  void Echo_Command(){
+    std::cout<<command.substr(5)<<std::endl;
+  }
+
+  void not_builtin(){
+    std::string system_path = std::getenv("path");
+    std::istringstream path_stream(system_path);
+      std::vector<std::string>paths_split;
+      std::string temp;
+
+      bool found = false;
+      while(std::getline(path_stream, temp, ';')){
+	std::string filepath = temp + "\\" + command.substr(5) + ".exe";
+	if(access(filepath.c_str(), X_OK) == 0){
+		std::cout << command.substr(5)<<" is "<<filepath<<std::endl;		    
+    found = true;
+		break;
+	}
+}
+  if(!found){
+    std::cout<<command.substr(5)<<": not found"<<std::endl;
+}
+  }
+
+  void Type_command(){
+    if(command.substr(5)=="exit" || command.substr(5)=="type" || command.substr(5)=="echo"){
+        std::cout<<command.substr(5)<<" is a shell builtin"<<std::endl;
+      }
+    else{
+      not_builtin();
+    }
+  }
+
+  void myCommand(){
+    while(true){
+      std::cout<<"$ ";
+      getline(std::cin,command);
+      if(command=="exit"){
+        break;
+      }
+      else if(command.size() >= 5 &&command.substr(0,5)=="echo "){
+        Echo_Command();
+      }
+      else if(command.size() >= 5 &&command.substr(0,5)=="type "){
+        Type_command();
+      }else{
+          std::cout<<command.substr(5)<<": not found\n";
+          continue;
+      }
+    }
+  }
+
+};
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -7,29 +71,9 @@ int main() {
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-  while(true){
-    std::cout<<"$ ";
-    std::string command;
-    std::getline(std::cin,command);
-    if(command=="exit"){
-      break;
-    }
-    else if(command.substr(0,5)=="echo "){
-      std::cout<<command.substr(5,command.size()-1)<<std::endl;
-    }
-
-    else if(command.substr(0,5)=="type "){
-        if(command.substr(5,command.size()-1)=="exit" || command.substr(5,command.size()-1)=="type" || command.substr(5,command.size()-1)=="echo"){
-          std::cout<<command.substr(5,command.size()-1)<<" is a shell builtin"<<std::endl;
-        }else{
-          std::cout<<command.substr(5,command.size()-1)<<": not found\n";
-          continue;
-        }
-    }
-    else{
-      std::cout<<command<<": command not found\n";
-    }
-  }
+  Shell* myObject = new Shell();
+  myObject->myCommand();
+  delete myObject;
 
   return 0;
 }
